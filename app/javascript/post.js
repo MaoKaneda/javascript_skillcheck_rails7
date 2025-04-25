b27342dfbbdca0ea826ac37b91b673ede6013db8
MaoKaneda <maddieqqie@gmail.com>
2025-04-25 16:33:02 +0900

    非同期で投稿した内容をブラウザ上で表示

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,6 +1,25 @@
 // 非同期投稿機能の実装
 function post (){
-  console.log("非同期投稿を実装");
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/articles", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      const item = XHR.response.article;
+      const contentsArea = document.getElementById("contents_area");
+      const articleText = document.getElementById("article_text");
+      const HTML = `
+        <div class="article">
+          ${ item.text }
+        </div>`;
+      contentsArea.insertAdjacentHTML("afterbegin", HTML);
+      articleText.value = "";
+    };
+    e.preventDefault();
+  });
 };
 
 // ページ読み込み時にpost関数を実行
